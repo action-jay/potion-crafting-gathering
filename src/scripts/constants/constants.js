9b28366d27de5fede3e427d6c31dfc376a3ee4dc
p4535992 <tentimarco0@gmail.com>
2023-08-16 22:19:33 +0200

    version 0.5.1 beta

---
+++ src/scripts/constants/constants.js
@@ -0,0 +1,10 @@
+const CONSTANTS = {
+  MODULE_ID: "potion-crafting-and-gathering",
+  PATH: `modules/potion-crafting-and-gathering/`,
+  PACK_UUID_ROLLTABLES: "potion-crafting-and-gathering.potion-crafting-and-gathering-tables",
+  PACK_UUID_JOURNALS: "potion-crafting-and-gathering.potion-crafting-and-gathering-journal",
+};
+
+CONSTANTS.PATH = `modules/${CONSTANTS.MODULE_ID}/`;
+
+export default CONSTANTS;
